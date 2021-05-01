cb92d5171a79acb24ebf0fb9e81de7543d77c35d
aha-ha <ahawespe@gmail.com>
2021-05-01 15:06:51 +0200

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,13 @@
+	function create_qr() {
+	var url = "http://api.qrserver.com/v1/create-qr-code/?data=";
+	var data = document.getElementById("data").value;
+	var size = document.getElementById("size").value;
+	var colortxt = document.getElementById("color").value;
+	var color = String(colortxt.replace("#", ""));
+	var colorbgtxt = document.getElementById("bgcolor").value;
+	var colorbg = String(colorbgtxt.replace("#", ""));
+	var format = document.getElementById("format").value;
+	var ecc = document.getElementById("ecc").value;
+	url = url + data + "&size=" + size + "&color=" + color + "&bgcolor=" + colorbg + "&format=" + format + "&ecc=" + ecc;
+	document.getElementById("dwnldlink").href = url;
+	}
