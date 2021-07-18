37458f01d0df7eaa97a5af9ea226d8c64a2367be
aha-ha <ahawespe@gmail.com>
2021-07-18 09:39:48 +0200

    Update main.js

--- 3d_Logo/main.js
+++ 3d_Logo/main.js
@@ -1,39 +1,75 @@
-window.onload = function(){
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-//camera 
-var camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 1, 1000); 
-camera.position.y = -250; 
-camera.position.z = 400; 
-camera.rotation.x = 45 * (Math.PI / 180); 
-//scene 
-var scene = new THREE.Scene();
-scene.add(camera); //ADDED
-var img = new THREE.MeshBasicMaterial({ //CHANGED to MeshBasicMaterial
-map = new THREE.TextureLoader('img/AracoLogo.png')
-});
-img.map.needsUpdate = true; //ADDED
-//plane
-var plane = new THREE.Mesh(new THREE.PlaneGeometry(200, 200),img);
-plane.overdraw = true;
-scene.add(plane);
-//add subtle ambient lighting
-var ambientLight = new THREE.AmbientLight(0x555555);
-scene.add(ambientLight);
-//add directional light source
-var directionalLight = new THREE.DirectionalLight(0xffffff);
-directionalLight.position.set(1, 1, 1).normalize();
-scene.add(directionalLight);
-//create wrapper object that contains three.js objects
-var three = {
-renderer: renderer,
-camera: camera,
-scene: scene,
-plane: plane
-};
+function main() {
+	var scene = new THREE.Scene();
+	var box = generateBox(15,15,15);
+	box.name = 'box-1';
+	box.translateZ(-30);
+	scene.add(box);
+	var pointLight = generatePointLight(0xffffff, 1);
+	pointLight.position.y = 5;
+	scene.add(pointLight);
+	var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 1000);
+	camera.position.x = 0;
+	camera.position.y = 5;
+	camera.position.z = 5;
+	camera.lookAt(new THREE.Vector3(0,0,-30))
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.getElementById('webgl').appendChild(renderer.domElement);
+	update(renderer, scene, camera);
+	return scene;
+	
+
+
+}
+	var scene = main();
+	 document.addEventListener("keydown", PressT);
+	function PressT (evt) {
+        var kc = evt.keyCode;
+
+	if (kc == 37) {
+		// Pfeil nach links
+		scene.getObjectByName('box-1').rotation.y -= 0.005;
+	} else if (kc == 38) {
+		// Pfeil nach oben
+		scene.getObjectByName('box-1').rotation.x -= 0.005;
+	} else if (kc == 39) {
+		// Pfeil nach rechts
+		scene.getObjectByName('box-1').rotation.y += 0.005;
+	} else if (kc == 40) {
+		// Pfeil Nach unten
+		scene.getObjectByName('box-1').rotation.x += 0.005;
+	}}
+
+
+function generatePointLight(color, intensity) {
+	return new THREE.PointLight(color, intensity);
+}
+
+function generateBox(w, h, d) {
+	var geo = new THREE.BoxGeometry(w, h, d);
+	var mat = new THREE.MeshPhongMaterial({
+		color: 'rgb(100,100,100)'
+	});
+	var mesh = new THREE.Mesh(geo, mat);
+	return mesh;
+}
+
+function update(renderer, scene, camera) {
+	renderer.render(scene, camera);
+
+	requestAnimationFrame(function(){
+		update(renderer, scene, camera)
+	})
 }
 
 
 
-// Test
+
+
+
+
+
+
+
+
+ console.log(scene);
